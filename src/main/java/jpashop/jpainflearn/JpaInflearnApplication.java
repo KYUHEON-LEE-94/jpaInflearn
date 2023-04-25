346ae7830d2f10d1957894b1a167adc39a1c8cbb
khlee <panpan68@naver.com>
2023-04-25 16:56:19 +0900

    Initial commit

---
+++ src/main/java/jpashop/jpainflearn/JpaInflearnApplication.java
@@ -0,0 +1,13 @@
+package jpashop.jpainflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpashop/jpainflearn/JpaInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package jpashop.jpainflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
